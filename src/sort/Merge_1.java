bd83c6e935e6bf526c3faee682a5bd1596aa1224
saowu <786021717@qq.com>
2020-04-01 13:29:43 +0800

    merge sort

---
+++ src/sort/Merge_1.java
@@ -0,0 +1,71 @@
+package sort;
+
+/**
+ * 自底向上的归并排序
+ */
+public class Merge_1 {
+
+    private static Comparable[] aux;
+
+    public static void sort(Comparable[] a) {
+        int N = a.length;
+        aux = new Comparable[a.length];
+        for (int sz = 1; sz < N; sz += sz) {
+            for (int lo = 0; lo < N - sz; lo += sz + sz) {
+                merge(a, lo, lo + sz - 1, Math.min(lo + sz + sz - 1, N - 1));
+            }
+        }
+    }
+
+    public static void merge(Comparable[] a, int lo, int mid, int hi) {
+        int i = lo;
+        int j = mid + 1;
+        for (int k = lo; k <= hi; k++) {
+            aux[k] = a[k];
+        }
+        for (int k = lo; k <= hi; k++) {
+            if (i > mid) {
+                a[k] = aux[j++];
+            } else if (j > hi) {
+                a[k] = aux[i++];
+            } else if (less(aux[j], aux[i])) {
+                a[k] = aux[j++];
+            } else {
+                a[k] = aux[i++];
+            }
+        }
+    }
+
+    public static void exch(Comparable[] a, int i, int j) {
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    public static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    public static boolean isSorted(Comparable[] a) {
+        for (int i = 1; i < a.length; i++) {
+            if (less(a[i], a[i - 1])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        String[] s = {"bed", "bug", "dad", "all", "bad", "yet", "zoo"};
+        sort(s);
+        assert isSorted(s);
+        show(s);
+    }
+}
