bdbb606429309ebf1bf5d88b8be9f08bc458b395
saowu <786021717@qq.com>
2020-04-11 18:31:36 +0800

    add Quick3way sort

---
+++ src/sort/Quick3way.java
@@ -0,0 +1,65 @@
+package sort;
+
+import edu.princeton.cs.algs4.StdRandom;
+
+/**
+ * 三向切分的快速排序算法
+ */
+public class Quick3way {
+
+    public static void sort(Comparable[] a) {
+        StdRandom.shuffle(a);
+        sort(a, 0, a.length - 1);
+    }
+
+    public static void sort(Comparable[] a, int lo, int hi) {
+        if (hi <= lo) return;
+        int lt = lo, i = lo + 1, gt = hi;
+        Comparable v = a[lo];
+        while (i <= gt) {
+            int cmp = a[i].compareTo(v);
+            if (cmp < 0) {
+                exch(a, lt++, i++);
+            } else if (cmp > 0) {
+                exch(a, i, gt--);
+            } else {
+                i++;
+            }
+        }
+        sort(a, lo, lt - 1);
+        sort(a, gt + 1, hi);
+    }
+
+
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    private static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
+    }
+
+    private static boolean isSorted(Comparable[] a) {
+        for (int i = 1; i < a.length; i++) {
+            if (less(a[i], a[i - 1]))
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String[] s = {"bed", "bug", "dad", "all", "bad", "yet", "zoo"};
+        sort(s);
+        assert isSorted(s);
+        show(s);
+    }
+}
